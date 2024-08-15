0e455b7183e1dea0b424e8a7f98848f0e12c80fb
oddlyspaced <oddlyspaced1@gmail.com>
2024-08-15 10:06:33 +0530

    chore: Config class

--- src/main/kotlin/Llama.kt
+++ src/main/kotlin/Llama.kt
@@ -1,3 +1,13 @@
+data class Config(
+    val dim: Int, // transformer dimension
+    val hiddenDim: Int, // for feed forward network layers
+    val layerCount: Int, // layer count
+    val headCount: Int, // query head count
+    val kvHeadCount: Int, // key / value head count (can be < query because of multiquery)
+    val vocabSize: Int, // vocabulary size, usually 256 (byte level)
+    val seqLen: Int, // max sequence length
+)
+
 class Llama {
 
 }
